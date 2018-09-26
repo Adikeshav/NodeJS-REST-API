5303fac97d92821b75f10133114661fd7f7eb23b
Adikeshav S <adikeshav.cse@gmail.com>
2018-09-26 17:21:46 +0530

    Restful API

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 /*
-
+*
+*
+*
 */
 
 //Dependencies
-var http = require("http");
-var url = require("url")
+let server = require('./lib/server');
+let workers = require('./lib/workers')
 
-//Create server to respond to the incoming requests
-var server = http.createServer(function(req, res){
-    //Get the URL and parse it
-    var parsedUrl = url.parse(req.url,true);
-    //Get path
-    var path = parsedUrl.pathname
-    var trimmedPath = path.replace(/^\/+|\/+$/g,'')
-    //Send response
-    res.end("Hello World\n")
-    //Log request path
-    console.log("Request recieved for path: "+trimmedPath)
-});
+//Declare App
+let app = {}
 
-//Start server and have it listen on port 3000
-server.listen(3000, function(){
-    console.log("Server is listening on port 3000")
-})
+//Initialise app
+app.init = function () {
+    server.init();
+    workers.init();
+}
+
+app.init(); //Execute the app
+
+//Export Module
+module.exports = app;
